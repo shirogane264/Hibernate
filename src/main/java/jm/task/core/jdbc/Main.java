db46ad99f8211caa48f9b9220a39a692f8a8db28
shirogane264 <143075830+shirogane264@users.noreply.github.com>
2024-01-11 14:32:22 +0500

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,21 +1,21 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.model.User;
 
 public class Main {
     public static void main(String[] args) {
-        UserDao userDao = new UserDaoHibernateImpl();
-        userDao.createUsersTable();
-        userDao.saveUser("Name1", "LastName1", (byte) 1);
-        userDao.saveUser("Name2", "LastName2", (byte) 2);
-        userDao.saveUser("Name3", "LastName3", (byte) 3);
-        userDao.saveUser("Name4", "LastName4", (byte) 4);
-        for (User user : userDao.getAllUsers()) {
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Name1", "LastName1", (byte) 1);
+        userService.saveUser("Name2", "LastName2", (byte) 2);
+        userService.saveUser("Name3", "LastName3", (byte) 3);
+        userService.saveUser("Name4", "LastName4", (byte) 4);
+        for (User user : userService.getAllUsers()) {
             System.out.println(user);
         }
-        userDao.cleanUsersTable();
-        userDao.dropUsersTable();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
